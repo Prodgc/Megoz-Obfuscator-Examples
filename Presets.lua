0d8b7cdca227d73d9e8c95f79af58c0881883f72
Prodgc <162835899+Prodgc@users.noreply.github.com>
2025-06-24 14:28:08 +0300

    Update Presets.lua

--- Presets.lua
+++ Presets.lua
@@ -1,4 +1,4 @@
 --// Ever Code In This File Generated With Terrame+ Obfuscator \\--
 
 --// Weak Preset \\-- [ still development, for sandbox etc.. ] [ watermark not working yet ]
-("This File Protected With Terrame+ Obfuscator"):gsub(".+","")local m={"pcall","load";"loadstring","unpack","string.byte","print"}local t={}for m,f in ipairs(m)do if f:find("%.")then local I,c=f:match("^(%w+)%.(%w+)$");(t)[m]=((_G)[I])[c]else(t)[m]=(_G)[f]end end local function f(m)local t={1,2;3,4;5;6}return(t)[m]end local I="test";(t)[f(6)](I)
+("This File Protected With Terrame+Obfuscator"):gsub(".+","")local f={"pcall","load","loadstring","unpack","string.byte","print"}local b={}for i,name in ipairs(f)do if name:find("%.")then local c,d=name:match("^(%w+)%.(%w+)$")b[i]=_G[c][d] else b[i]=_G[name] end end local function a(i)local e={((4*8)-2)+-29,((6+8)*2)+-26,((2-8)*5)+33,((3*3)*7)+-59,((6*6)+9)+-40,((1-4)+6)+3}return e[i] end local f="test"b[a(6)](f)
