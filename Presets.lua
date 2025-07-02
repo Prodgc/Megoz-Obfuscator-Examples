b867848c61c6b2bedb1831a4e89b1df498546b54
Prodgc <162835899+Prodgc@users.noreply.github.com>
2025-07-02 16:11:50 +0300

    Update Presets.lua

--- Presets.lua
+++ Presets.lua
@@ -1,4 +1,5 @@
---// Ever Code In This File Generated With Terrame+ Obfuscator \\--
-
---// Weak Preset \\-- [ still development, for sandbox etc.. ] [ watermark not working yet ]
-("This File Protected With Terrame+Obfuscator"):gsub(".+","")local f={"pcall","load","loadstring","unpack","string.byte","print"}local b={}for i,name in ipairs(f)do if name:find("%.")then local c,d=name:match("^(%w+)%.(%w+)$")b[i]=_G[c][d] else b[i]=_G[name] end end local function a(i)local e={((4*8)-2)+-29,((6+8)*2)+-26,((2-8)*5)+33,((3*3)*7)+-59,((6*6)+9)+-40,((1-4)+6)+3}return e[i] end local f="test"b[a(6)](f)
+-- This File Generated With Terrame+ Obfuscator
+-- ID: A98754-B98665-C98762-PFDCKMZX  
+-- Preset: Weak
+-- Features: Anti Hook
+("Protected With Niggfuscator 0.0.4"):gsub(".+","...")local K={"7063616c6c","6c6f6164";"6c6f6164737472696e67";"756e7061636b";"737472696e672e62797465","7072696e74"}K=(({function(K)local m=function(K)local m=""for Y=1,#K,2 do m=m..string.char(tonumber(K:sub(Y,Y+1),16))end return m end for Y=1,#K,1 do(K)[Y]=m((K)[Y])end return K end}))[1](K)local m={}for K,Y in ipairs(K)do if Y:find("%.")then local j,E=Y:match("^(%w+)%.(%w+)$");(m)[K]=((_G)[j])[E]else(m)[K]=(_G)[Y]end end local function Y(K)local m={(3*1)+-2,(4*1)+-2,(1*4)+-1,(4*2)+-4,(2*3)+-1,(1*1)+5}return(m)[K]end table.concat=function()return"tamper detected"end local j=function()(m)[Y(6)]((function(K)local m=""for Y=#K,1,-1 do m=m..K:sub(Y,Y)end return m end)("metsys dessap"))end local E=dumper dumper=function(K)if K==j then return""else return E(K)end end pcall=function()return(m)[Y(6)]((function(K)local m=""for Y=#K,1,-1 do m=m..K:sub(Y,Y)end return m end)("rorre emitnur"))end j();(m)[Y(6)]((function(K)local m=""for Y=#K,1,-1 do m=m..K:sub(Y,Y)end return m end)("dlroW ,olleH"))
