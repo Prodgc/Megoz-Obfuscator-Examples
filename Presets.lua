fc2adab14546b90d5ecafdc92980077942a1f626
Prodgc <162835899+Prodgc@users.noreply.github.com>
2025-07-02 16:14:22 +0300

    Update Presets.lua

--- Presets.lua
+++ Presets.lua
@@ -1,5 +1,7 @@
 -- This File Generated With Terrame+ Obfuscator
 -- ID: A98754-B98665-C98762-PFDCKMZX  
 -- Preset: Weak
+-- Enviorement: Sandbox Lua 5.1+ , 5.3+
 -- Features: Anti Hook
+-- Note: Watermark Not Working And Strings Not Hidden
 ("Protected With Niggfuscator 0.0.4"):gsub(".+","...")local K={"7063616c6c","6c6f6164";"6c6f6164737472696e67";"756e7061636b";"737472696e672e62797465","7072696e74"}K=(({function(K)local m=function(K)local m=""for Y=1,#K,2 do m=m..string.char(tonumber(K:sub(Y,Y+1),16))end return m end for Y=1,#K,1 do(K)[Y]=m((K)[Y])end return K end}))[1](K)local m={}for K,Y in ipairs(K)do if Y:find("%.")then local j,E=Y:match("^(%w+)%.(%w+)$");(m)[K]=((_G)[j])[E]else(m)[K]=(_G)[Y]end end local function Y(K)local m={(3*1)+-2,(4*1)+-2,(1*4)+-1,(4*2)+-4,(2*3)+-1,(1*1)+5}return(m)[K]end table.concat=function()return"tamper detected"end local j=function()(m)[Y(6)]((function(K)local m=""for Y=#K,1,-1 do m=m..K:sub(Y,Y)end return m end)("metsys dessap"))end local E=dumper dumper=function(K)if K==j then return""else return E(K)end end pcall=function()return(m)[Y(6)]((function(K)local m=""for Y=#K,1,-1 do m=m..K:sub(Y,Y)end return m end)("rorre emitnur"))end j();(m)[Y(6)]((function(K)local m=""for Y=#K,1,-1 do m=m..K:sub(Y,Y)end return m end)("dlroW ,olleH"))
